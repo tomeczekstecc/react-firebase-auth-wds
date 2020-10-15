b894547da898228659728fd72d7bbcf72005200f
Tomasz Steć <tomeczekstecc@gmail.com>
2020-10-15 23:19:48 +0200

    vars

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 
 const app = firebase.initializeApp({
-  apiKey: 'AIzaSyCip9iH16CHKXyINsPOG5XBk58C0wQkbLE',
-  authDomain: 'react-firebase-auth-wds-dev.firebaseapp.com',
-  databaseURL: 'https://react-firebase-auth-wds-dev.firebaseio.com',
-  projectId: 'react-firebase-auth-wds-dev',
-  storageBucket: 'react-firebase-auth-wds-dev.appspot.com',
-  messagingSenderId: '1062927417553',
-  appId: '1:1062927417553:web:31a87591a9c96bb82185f0',
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSANGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 });
 
 
